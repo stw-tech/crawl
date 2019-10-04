ff7706ae9e99b5a5b3d6618de59b4c38cf13d9e7
conrad <r06942143@ntu.edu.tw>
2019-10-05 00:43:30 +0800

    fix_PlanNo_Pro

--- grb/table_grb.py
+++ grb/table_grb.py
@@ -18,33 +18,36 @@ def _ToString(data):
         return data
 
 for grb_data in glob.glob("./data/*/*.json"):
-    with open(grb_data , 'r',encoding = 'utf-8') as reader:
-        jf = json.loads(reader.read())
-    table.put_item(
-    Item={
-            "abstractC": _ToNull(jf['abstractC']),
-            "abstractE":  _ToNull(jf['abstractE']),
-            "excuOrganName":  _ToNull(jf['excuOrganName']),
-            "host1NameC":  _ToNull(jf['host1NameC']),
-            "hostNameList":  _ToNull(jf['hostNameList']),
-            "id":  _ToNull(jf['id']),
-            "keyword":  _ToNull(jf['keyword']),
-            "keywordC":  _ToNull(jf['keywordC']),
-            "keywordE":  _ToNull(jf['keywordE']),
-            "pengDesc":  _ToNull(jf['pengDesc']),
-            "periodEnym":  _ToNull(jf['periodEnym']),
-            "periodStym":  _ToNull(jf['periodStym']),
-            "planAmt":  _ToNull(jf['planAmt']),
-            "planNo":  _ToString(jf['planNo']),
-            "planOrganCode":  _ToNull(jf['planOrganCode']),
-            "planOrganName":  _ToNull(jf['planOrganName']),
-            "planYear":  _ToNull(jf['planYear']),
-            "pnchDesc":  _ToNull(jf['pnchDesc']),
-            "projkey":  _ToNull(jf['projkey']),
-            "researchAttribute":  _ToNull(jf['researchAttribute']),
-            "researchField1":  _ToNull(jf['researchField1']),
-            "researchField2":  _ToNull(jf['researchField2']),
-            "researchType":  _ToNull(jf['researchType']),
-            "title":  _ToNull(jf['title'])
-        }
-    )
+    try:
+        with open(grb_data , 'r',encoding = 'utf-8') as reader:
+            jf = json.loads(reader.read())
+        table.put_item(
+        Item={
+                "abstractC": _ToNull(jf['abstractC']),
+                "abstractE":  _ToNull(jf['abstractE']),
+                "excuOrganName":  _ToNull(jf['excuOrganName']),
+                "host1NameC":  _ToNull(jf['host1NameC']),
+                "hostNameList":  _ToNull(jf['hostNameList']),
+                "id":  _ToNull(jf['id']),
+                "keyword":  _ToNull(jf['keyword']),
+                "keywordC":  _ToNull(jf['keywordC']),
+                "keywordE":  _ToNull(jf['keywordE']),
+                "pengDesc":  _ToNull(jf['pengDesc']),
+                "periodEnym":  _ToNull(jf['periodEnym']),
+                "periodStym":  _ToNull(jf['periodStym']),
+                "planAmt":  _ToNull(jf['planAmt']),
+                "planNo":  _ToString(jf['planNo']),
+                "planOrganCode":  _ToNull(jf['planOrganCode']),
+                "planOrganName":  _ToNull(jf['planOrganName']),
+                "planYear":  _ToNull(jf['planYear']),
+                "pnchDesc":  _ToNull(jf['pnchDesc']),
+                "projkey":  _ToNull(jf['projkey']),
+                "researchAttribute":  _ToNull(jf['researchAttribute']),
+                "researchField1":  _ToNull(jf['researchField1']),
+                "researchField2":  _ToNull(jf['researchField2']),
+                "researchType":  _ToNull(jf['researchType']),
+                "title":  _ToNull(jf['title'])
+            }
+        )
+    except:
+        print(_ToNull(jf['id']))
