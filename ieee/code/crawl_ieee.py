f216f966ae45543e408bc89f105e63d510461cce
lisyuan <lisyuan0429@gmail.com>
2019-11-20 15:10:34 +0800

    final version: crawl conference and journal from 1975 to 2019

--- ieee/code/crawl_ieee.py
+++ ieee/code/crawl_ieee.py
@@ -3,15 +3,13 @@ import json
 import time
 import os
 import random
-from collections import Counter
 from bs4 import BeautifulSoup
 import re
+import time
 
 
-
-
-start_year, end_year = 1975, 2017
-paper_type = ['conference', 'journal']
+start_year, end_year = 1975, 2019
+paper_type = ['journal', 'conference']
 
 start_page = 1
 request_url = 'https://ieeexplore.ieee.org/rest/search'
@@ -34,30 +32,46 @@ payload = {
             "sortType": "newest"
            }
 
-proxy_list = ['104.236.248.219:3128', '167.71.249.181:8888']
+updating_dir = '../updating_data/'
+checking_dir = '../data'
+
+proxy_list = ['167.71.106.246:3128', '165.22.123.198:8080', '157.245.90.37:8080']
 
 def crawl_ieee(paper_list, p_type, year):
 
+    '''
+    this function crawls the paper in metadata
+
+    Args:
+        paper_list: The list of paper id should be crawled
+        p_type: conference or journal
+        year: year
+    '''
+
     print(">>> crawling {} {} paper".format(year, p_type))
+    count = 0
     for id in paper_list:
 
         doc_link = 'https://ieeexplore.ieee.org/document/' + id
 
         while True:
             try:
-                time.sleep(1)
+                time.sleep(random.uniform(1, 3))
                 proxies = get_proxies()
                 doc_res = requests.get(doc_link, proxies = proxies)
 
-            except requests.exceptions.ConnectionError:
-                print('requests.exceptions.ConnectionError: stop for 60s ')
-                time.sleep(30)
+            except requests.exceptions.ConnectionError as error:
+                print('{}: stop for 20s; proxy ip: {};'.format(error, proxies))
+                time.sleep(20)
+                continue
+            except requests.exceptions.ProxyError:
+                print('proxy error: stop for 10s')
+                time.sleep(10)
                 continue
             break
 
         html_txt = doc_res.text
         soup = BeautifulSoup(html_txt, 'lxml')
-        # print(soup.prettify())
         tag = soup.find_all('script', type="text/javascript", src ='')
 
         for t in tag  :
@@ -73,20 +87,30 @@ def crawl_ieee(paper_list, p_type, year):
                     metadata_dict = json.loads(metadata)
                     if metadata_dict != None:
                         save_json(metadata_dict, p_type, year)
+                        count += 1
                     else:
-                        print('meta_data is None')
+                        print('meta_data is empty')
                 except json.decoder.JSONDecodeError:
                     print('metadata decode error')
 
-    print('finishing crawling')
+        print('finishing crawling {}/{}'.format(count, len(paper_list)))
 
 
 def save_json(crawl_data, p_type, year):
 
-    try:
-        path = os.path.join('../data/', p_type, str(year), crawl_data['articleNumber'])
-    except TypeError:
-        print('crawl data type error')
+    '''
+    this function saves the json
+
+    Args:
+        crawl_data: The crawling of IEEE data
+        p_type: conference or journal
+        year: year
+    '''
+
+    year_name = time.strftime("%Y", time.localtime())
+    date_name = time.strftime("%m%d", time.localtime())
+
+    path = os.path.join(updating_dir, year_name, date_name, p_type, str(year), crawl_data['articleNumber'])
 
     if not os.path.exists(path):
         os.makedirs(path)
@@ -97,75 +121,133 @@ def save_json(crawl_data, p_type, year):
 
 def check_crawled(p_type, year):
 
+    '''
+    this function checks the paper crawled or not
+
+    Args:
+        p_type: journal or conference
+        year: the year of the published papers
+
+    Returns:
+        paper_list: the list of papers should be crawled
+    '''
+
     print('>>> checking {} {} paper'.format(year, p_type))
+    start_time = time.time()
+
     payload['pageNumber'] = start_page
     payload['searchWithin'] = ["\"Publication Title\":{}".format(p_type), "\"Publication_Year\":{}".format(year)]
 
-    ck_dir = '../data'
-    ck_path = os.path.join(ck_dir, p_type, str(year))
+    ck_path = os.path.join(checking_dir, p_type, str(year))
 
     if not os.path.exists(ck_path):
         crawled_paper = []
     else:
         crawled_paper = os.listdir(ck_path)
+
+    if os.path.exists(updating_dir):
+
+        year_dir = os.listdir(updating_dir)
+        for y in year_dir:
+            y_path = os.path.join(updating_dir, y)
+
+            for date in os.listdir(y_path):
+                path = os.path.join(y_path, date, p_type, str(year))
+                if os.path.exists(path):
+                    id = os.listdir(path)
+                    crawled_paper.extend(id)
     while True:
         try:
             proxies = get_proxies()
             res = requests.post(url=request_url, data=json.dumps(payload), headers=headers, proxies = proxies)
             total_num = res.json()['totalRecords']
             total_page = res.json()['totalPages']
-        except json.decoder.JSONDecodeError:
-            print('json decode error')
+
+            if total_page == (total_num // payload['rowsPerPage'] + 1):
+                sort_type = ['newest']
+            else:  # the up-limited page number is 1000, so crawls the data by change the order into 'newest' and 'oldest'
+                sort_type = ['newest', 'oldest']
+                total_page = total_num // payload['rowsPerPage'] // 2 + 1
+        except :
             continue
         break
 
+
     if len(crawled_paper) < total_num:
+
         total_paper = []
-        while payload['pageNumber'] <= total_page:
+        for sort in sort_type:
+            payload['sortType'] = sort
+            payload['pageNumber'] = start_page
 
-            while True:
-                try:
-                    time.sleep(random.uniform(2, 6))
-                    proxies = get_proxies()
-                    res = requests.post(url=request_url, data=json.dumps(payload), headers=headers, proxies = proxies)
-                    res_json = res.json()
-                    paper_per_page = len(res_json['records'])
-                except json.decoder.JSONDecodeError:
-                    print('json decode error')
-                    time.sleep(60)
-                    continue
-                except KeyError:
-                    print('KeyError')
-                    time.sleep(60)
-                    continue
-                break
+            while payload['pageNumber'] <= total_page:
 
-            for idx in range(paper_per_page):
-                paper = res_json['records'][idx]
-                paper_id = paper['articleNumber']
-                total_paper.append(paper_id)
+                while True:
+                    try:
+                        time.sleep(random.uniform(2, 3))
+                        proxies = get_proxies()
+                        res = requests.post(url=request_url, data=json.dumps(payload), headers=headers, proxies = proxies)
+                        res_json = res.json()
+                        paper_per_page = len(res_json['records'])
+                    except json.decoder.JSONDecodeError :
+                        print('json decode error; proxy ip: {}; res status: {} --> stop for 20s'.format(proxies, res.status_code))
+                        time.sleep(20)
+                        continue
+                    except requests.exceptions.ProxyError:
+                        print('proxy error, ip: {}; res status: {} --> stop for 10s'.format(proxies, res.status_code))
+                        time.sleep(10)
+                        continue
+                    except requests.exceptions.ChunkedEncodingError:
+                        print('Chunked Encoding Error, ip: {} --> stop for 20s'.format(proxies))
+                        time.sleep(20)
+                        continue
+                    except requests.exceptions.SSLError:
+                        print('SSLError, ip: {} --> stop for 20s'.format(proxies))
+                        time.sleep(20)
+                        continue
 
-            payload['pageNumber'] += 1
+                    break
 
+                for idx in range(paper_per_page):
+                    paper = res_json['records'][idx]
+                    paper_id = paper['articleNumber']
+                    total_paper.append(paper_id)
+
+                payload['pageNumber'] += 1
+
+        total_paper = list(set(total_paper))
         paper_list = list(set(total_paper) - set(crawled_paper))
-        print('total num of crawling data :{}, finish crawling: {}, need to be crawled: {}'.format(len(total_paper), len(crawled_paper), len(paper_list)))
+        end_time = time.time()
+        check_time = end_time - start_time
+
+        print('total num of crawling data :{}, finish crawling: {}, need to be crawled: {}, checking time:{:.0f}h:{:.0f}m'
+              .format(len(total_paper), len(crawled_paper), len(paper_list), check_time//3600, (check_time//60)%60))
 
     else:
         print('Don\'t need to update')
         paper_list = []
+
     return paper_list
 
 def get_proxies():
+
+    '''
+    this function picks the proxy randomly
+
+    Returns:
+        proxies: proxy
+    '''
 
     index = random.randint(0, len(proxy_list) - 1)
     proxies = {'https': proxy_list[index]}
 
     return proxies
+
 if __name__ == "__main__":
 
     for type in paper_type:
 
         for year in range(end_year, start_year-1, -1):
             paper_list = check_crawled(type, year)
-            if len(paper_list) != 0:
+            if paper_list:
                 crawl_ieee(paper_list, type, year)
